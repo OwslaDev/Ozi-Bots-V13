8724ff100c6c7a8152fd746735f3d2d064f252c0
Owsla <107294999+OwslaDev@users.noreply.github.com>
2022-06-19 14:34:56 +0400

    Update ozi.js

--- ozi.js
+++ ozi.js
@@ -1,2 +1,3 @@
 Bu bot market sistemi dahil olmak üzere geliştirilip v13 hale sorunsuz getirilmiştir almak isteyenler Discord: Jaylen ⚚#0001 den fiyat bilgilendirme alabilirler. 
 S4tıldıktan sonra bu bot asla paylaşılmayacaktır.
+ çalışmıyor reyiz discord
